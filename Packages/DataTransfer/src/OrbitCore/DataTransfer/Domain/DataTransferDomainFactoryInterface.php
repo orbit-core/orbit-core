8482a9ec5ed174ea401696cc73866865f12b6fb9
Mike Bertram <contact@mibexx.de>
2020-01-08 16:39:34 +0100

    * Factory update

--- Packages/DataTransfer/src/OrbitCore/DataTransfer/Domain/DataTransferDomainFactoryInterface.php
+++ Packages/DataTransfer/src/OrbitCore/DataTransfer/Domain/DataTransferDomainFactoryInterface.php
@@ -4,7 +4,12 @@ declare(strict_types=1);
 namespace OrbitCore\DataTransfer\Domain;
 
 
+use OrbitCore\DataTransfer\Domain\Builder\BuilderFactoryInterface;
+use OrbitCore\DataTransfer\Domain\Builder\TransferBuilderInterface;
+
 interface DataTransferDomainFactoryInterface
 {
+    public function createBuilderFactory(): BuilderFactoryInterface;
 
+    public function createTransferBuilder(): TransferBuilderInterface;
 }
